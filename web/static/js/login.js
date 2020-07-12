a991a533b38537ece636fc18bb9c379dd881aca6
mayra-diaz <mayra.diaz@utec.edu.pe>
2020-07-12 01:33:00 -0500

    Basic login js

--- web/static/js/login.js
+++ web/static/js/login.js
@@ -5,3 +5,11 @@ const container = document.getElementById('container');
 signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
 
 signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
+
+function sign_up(){
+
+}
+
+function login(){
+
+}
