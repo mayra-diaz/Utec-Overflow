f8bb19ebac041a2ca1acc6e199af7b22d1e9378c
Alexandra-SR <alexandra.shulca@utec.edu.pe>
2020-07-21 23:48:55 -0500

    APP login and register

--- app/app/src/main/java/com/example/utecoverflow/MainActivity.java
+++ app/app/src/main/java/com/example/utecoverflow/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.utecoverflow;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onRegisterClicked(View view) {
+        Intent intent = new Intent(this, RegisteruserActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/app/src/main/java/com/example/utecoverflow/RegisteruserActivity.java
@@ -0,0 +1,14 @@
+package com.example.utecoverflow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisteruserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registeruser);
+    }
+}
