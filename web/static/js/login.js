9ca7891337e0fd110c4b61c2a6155a4633ce4a1b
PaoloMorey <paolo.morey@utec.edu.pe>
2020-07-23 10:57:35 -0500

    hola

--- web/static/js/login.js
+++ web/static/js/login.js
@@ -1,53 +1,23 @@
 function putView(){
-  // ajax
-  if(!logged){
-    template = "<div class=\'container\' id=\'container\'>	<div class=\'form-container sign-up-container\'>"
-    template += "<!--Crear un nuevo usuario--> <h1>Crear Cuenta</h1>"
-    			<div class="social-container">	</div><span></span>
-    			<input id="usernameCreate" type="text" placeholder="Usuario" />
-          <input id="nameCreate" type="text" placeholder="Nombre" />
-    			<input id="fullnameCreate" type="text" placeholder="Apellido" />
-    			<input id="passwordCreate" type="password" placeholder="Contraseña" />
-    			<button id="PostUserButton" onclick='button_press_Create();'>Crear Cuenta</button>
-        <!--Crear un nuevo usuario-->
-    	</div>
-    	<div class="form-container sign-in-container">
-        <!--Login del usuario-->
-    		<form>
-          <img src="/static/images/utecLogoSmall.png" alt="">
-
-    			<div class="social-container">
-
-    			</div>
-    			<span></span>
-    			<input id="usernameLogin" type="username" placeholder="Usuario" />
-    			<input id="passwordLogin" type="password" placeholder="Contraseña" />
-
-    			<button id="LoginButton" onclick='button_press_Login();'>Iniciar Sesión</button>
-    		</form>
-        <!--Login del usuario-->
-    	</div>
-    	<div class="overlay-container">
-    		<div class="overlay">
-    			<div class="overlay-panel overlay-left">
-    				<h1>¡Bienvenido de Vuelta!</h1>
-    				<p>Para ingresar y ver todas las novedades regístrese usando su cuenta</p>
-    				<button class="ghost" id="signIn">Iniciar Sesión</button>
-    			</div>
-    			<div class="overlay-panel overlay-right">
-    				<h1>¿No Está Registrado?</h1>
-    				<p>Cree una cuenta para empezar a resolver sus dudas</p>
-    				<button class="ghost" id="signUp">Crear Cuenta</button>
-    			</div>
-    		</div>
-    	</div>
-    </div>"
-
-    $.("#all_web").append(login_view)
-  }
-  else{
-
-  }
+    $("#all_web").empty();
+    template = "<div class=\'container\' id=\'container\'>	<div class=\'form-container sign-up-container\'>";
+    template += "<!--Crear un nuevo usuario--> <h1>Crear Cuenta</h1>";
+    template += "<div class=\'social-container\'></div><span></span>";
+    template += "<input id=\'usernameCreate\' type=\'text\' placeholder=\'Usuario\' />";
+    template += "<input id=\'nameCreate\' type=\'text\' placeholder=\'Nombre\' />";
+    template += "<input id=\'fullnameCreate\' type=\'text\' placeholder=\'Apellido\' />";
+    template += "<input id=\'passwordCreate\' type=\'password\' placeholder=\'Contraseña\' />";
+    template += "<button id=\'PostUserButton\' onclick='button_press_Create();'>Crear Cuenta</button>";
+    template += "<!--Crear un nuevo usuario-->";
+    template += "</div> <div class=\'form-container sign-in-container\'>";
+    template += "<!--Login del usuario--><form><img src=\'/static/images/utecLogoSmall.png\' alt=""><div class=\'social-container\'></div><span></span>";
+    template += "<input id=\'usernameLogin\' type=\'username\' placeholder=\'Usuario\' />";
+    template += "<input id=\'passwordLogin\' type=\'password\' placeholder=\'Contraseña\' />";
+    template += "<button id=\'LoginButton\' onclick='button_press_Login();'>Iniciar Sesión</button></form><!--Login del usuario--></div>";
+    template += "<div class=\'overlay-container\'><div class=\'overlay\'><div class=\'overlay-panel overlay-left\'><h1>¡Bienvenido de Vuelta!</h1><p>Para ingresar y ver todas las novedades regístrese usando su cuenta</p><button class=\'ghost\' id=\'signIn\'>Iniciar Sesión</button>";
+    template += "</div><div class=\'overlay-panel overlay-right\'><h1>¿No Está Registrado?</h1><p>Cree una cuenta para empezar a resolver sus dudas</p>";
+    template += "<button class=\'ghost\' id=\'signUp\'>Crear Cuenta</button></div></div></div></div>";
+    $("#all_web").append(template);
 }
 
 
